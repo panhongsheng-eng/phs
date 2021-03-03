74680ecf5904322d9785c656001732ea1850528e
panhongsheng-eng <79436937+panhongsheng-eng@users.noreply.github.com>
2021-03-03 10:18:01 +0800

    Add files via upload

---
+++ JavaNumber.java
@@ -0,0 +1,9 @@
+public class JavaNumber {
+    //一般的情况下我们会使用数据的最基本的类型；byte int short long double float boolean char
+    //对应包的类型有：Byte integer Short Long Float Character Boolean
+    public static void main(String[] args) {
+        Integer x = 5;
+        x= x+10;
+        System.out.println(x);
+    }
+}
